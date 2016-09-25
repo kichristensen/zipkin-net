163a953278416b5cd729c25ebaa17d192ccf5453
Kim Christensen <kich@mvno.dk>
2016-09-25 21:21:28 +0200

    Extract helper module into seperate file

--- src/zipkin-net/DataStructures.fs
+++ src/zipkin-net/DataStructures.fs
@@ -2,17 +2,6 @@
 
 open System
 
-[<AutoOpen>]
-module Helpers =
-    let unixBaseTime = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)
-    type DateTimeOffset with
-        member self.UnixTimeMicroseconds =
-            (self - unixBaseTime).TotalMilliseconds * 1000.0 |> int64
-
-    type TimeSpan with
-        member self.TotalMicroseconds =
-            self.TotalMilliseconds * 1000.0 |> int64
-
 [<AbstractClassAttribute; SealedAttribute>]
 type ZipkinConstants private() =
     static member ClientSend = "cs"
---
+++ src/zipkin-net/Helpers.fs
@@ -0,0 +1,14 @@
+﻿namespace ZipkinNet
+
+open System
+
+[<AutoOpen>]
+module Helpers =
+    let unixBaseTime = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)
+    type DateTimeOffset with
+        member self.UnixTimeMicroseconds =
+            (self - unixBaseTime).TotalMilliseconds * 1000.0 |> int64
+
+    type TimeSpan with
+        member self.TotalMicroseconds =
+            self.TotalMilliseconds * 1000.0 |> int64
